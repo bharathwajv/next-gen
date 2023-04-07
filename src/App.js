5645d91f6f3044c8080cc1b2b9ee4d011f18c9fe
bharathwajv <bharathwaj.vasu@gmail.com>
2023-04-07 21:36:28 +0530

    initial themes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
-import logo from './logo.svg';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { CssBaseline } from '@mui/material';
 import './App.css';
 import FloatingNav from './FloatingNav';
 
+const theme = createTheme({
+  typography: {
+    fontFamily: 'Montserrat, sans-serif',
+  },
+  palette: {
+    primary: {
+      main: '#353666',
+    },
+    secondary: {
+      main: '#707193',
+    },
+    background: {
+      default: '#222344',
+    },
+    text: {
+      primary: '#F2E9DC',
+    },
+    action: {
+      active: '#EC5656',
+    },
+    button: {
+      main: '#EC5656',
+    },
+  },
+});
+
 function App() {
   return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
       <FloatingNav />
+    </ThemeProvider>
   );
 }
 
